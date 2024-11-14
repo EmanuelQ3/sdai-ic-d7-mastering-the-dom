b23bf9c2d607b2099034ff40ff61138227fefebd
EmanuelQ3 <emanuel.quezada@hotmail.com>
2024-11-14 04:49:23 +0000

    added some JS code to hide cat cards

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// Write your JavaScipt code below this line. 
+// Write your JavaScipt code below this line.
+
+const mainTitle = document.getElementById("main-title")
+mainTitle.textContent = "Welcome to our sneeze-inducing furrball adoption center"
+
+const description = document.getElementById("description")
+description.textContent = "Find your purrrfect dust pillow!"
+
+const cat1Button = document.getElementById("cat-1-button")
+cat1Button.addEventListener("click", () => {
+    const whiskersCard = document.getElementById("cat-1")
+    whiskersCard.remove()
+    alert("Congrats on your new best friend Whiskers!")
+})
+
+const cat2Button = document.getElementById("cat-2-button")
+cat2Button.addEventListener("click", () => {
+    const daisyCard = document.getElementById("cat-2")
+    daisyCard.remove()
+    alert("Congrats on your new best friend Daisy!")
+})
+
+const cat3Button = document.getElementById("cat-3-button")
+cat3Button.addEventListener("click", () => {
+    const terryCard = document.getElementById("cat-3")
+    terryCard.remove()
+    alert("Congrats on your new best friend Terry!")
+})
+
